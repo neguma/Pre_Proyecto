e9e1a9bdcaaf7148edea8e6106a04a2dccd195ec
neguma <guma.nicolas@gmail.com>
2021-12-09 18:48:26 -0300

    commit2

--- AppProyecto/models.py
+++ AppProyecto/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Guitarra(models.Model):
+    marca = models.CharField(max_length=20)
+    modelo = models.CharField(max_length=20)
+    precio = models.IntegerField()
+class Cuerdas(models.Model):
+    marca = models.CharField(max_length=20)
+    modelo = models.CharField(max_length=20)
+    grosor = models.BooleanField()
+class Mastil(models.Model):
+    madera = models.CharField(max_length=20)
+    cant_trastes = models.IntegerField()
+    largo = models.IntegerField() 
